a478dbc6afc8e80e9f3d1ffb3b393e0df9ccc91d
Frederico Mozzato <frederico.smozzato@gmail.com>
2025-01-28 07:30:40 -0300

    Set tab to 2 spaces

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,6 +18,9 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+-- set tab size
+vim.opt.tabstop = 2
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
